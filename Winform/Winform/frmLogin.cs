c9591357aed4ba5c943e000be7acb2e23be76c98
px4 <px4.vnd@gmail.com>
2023-10-12 10:24:07 +0700

    Create Project

---
+++ Winform/Winform/frmLogin.cs
@@ -0,0 +1,52 @@
+﻿using DevExpress.XtraEditors;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Winform
+{
+    public partial class frmLogin : DevExpress.XtraEditors.XtraForm
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+        bool checkInput()
+        {
+            if(txtUser.Text == "" || txtPass.Text == "")
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if(!checkInput())
+            {
+                MessageBox.Show("User or Pass must have values", "Error input", MessageBoxButtons.OK);
+                return;
+            }
+            if(!checkAccount(txtUser.Text, txtPass.Text))
+            {
+                MessageBox.Show("User or Pass not correct", "Login Failed", MessageBoxButtons.OK);
+                return;
+            }
+            MessageBox.Show("Welcom to the application", "Login Success", MessageBoxButtons.OK);
+            this.DialogResult = DialogResult.OK;
+        }
+
+
+        bool checkAccount(string user, string pass)
+        {
+            if(user == "a" && pass == "b") return true;
+            return false;
+        }
+    }
+}
---
+++ Winform/Winform/frmMain.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Winform
+{
+    public partial class frmMain : DevExpress.XtraEditors.XtraForm
+    {
+        public frmMain()
+        {
+            /*
+                frmLogin lg = new frmLogin();
+                lg.ShowDialog();
+                if(lg.DialogResult != DialogResult.OK)
+                {
+                    Environment.Exit(0);
+                } 
+            */
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
